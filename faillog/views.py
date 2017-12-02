87c9dd5ac8cabdabecd7df6cebf63f2744d68b3e
William Zhang <willzhang05@gmail.com>
2017-12-02 11:26:22 -0500

    add initial flask app

---
+++ faillog/views.py
@@ -0,0 +1,6 @@
+from faillog import app
+from flask import render_template
+
+@app.route('/')
+def index():
+        return render_template('index.html')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='faillog',
+    packages=['faillog'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
