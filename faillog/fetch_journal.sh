f9abf8cee1b29641a510238ea7e647d56816e6d8
William Zhang <willzhang05@gmail.com>
2017-12-02 20:40:01 -0500

    fix sdcript

--- faillog/fetch_journal.sh
+++ faillog/fetch_journal.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-echo -n > sshd.json
-while true 
-    do
-        comm -23 <(journalctl -u sshd --no-pager -o json -n 10 | sort) <(sort sshd.json) > sshd.json
+touch sshd.json
+journalctl -q -u sshd --no-pager -o json -n 1 > sshd.json
+TIME=$( date +"%T" )
+while true
+do
+        LENGTH=$(fgrep -o "{" sshd.json | wc -l)
+        NEWDATA=$(journalctl -q -u sshd --no-pager -o json --since $TIME)
+        DELTA=$(echo $NEWDATA | fgrep -o "{" | wc -l)
+        if [[ $DELTA -gt 10 ]]; then
+            echo "big"
+            echo $NEWDATA >> sshd.json
+            TIME=$( date +"%T" )
+        fi
+        echo $LENGTH
+        echo $DELTA
         sleep 5
+        clear
+        
 done
--- faillog/views.py
+++ faillog/views.py
@@ -9,13 +9,15 @@ def index():
 
 @app.route('/api')
 def api():
+    out = []
     with open('./sshd.json', 'r') as f: 
         addresses = set()
-        out = []
+
         for line in f:
             print(line)
+            print()
             data = json.loads(line)
             #if re.search(r'\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b', data['MESSAGE']):
                 #and not re.search(r'^(?:10|127|172\.(?:1[6-9]|2[0-9]|3[01])|192\.168)\..*', data['MESSAGE']):
-            out.append(data['MESSAGE'])
-        return ''.join(out)
+            out.append('<span style="display:inline-block; width:100%;">' + data['MESSAGE'] + '</span>')
+    return ''.join(out)
