2034c7a851ad2b80017556b88094649d13f71a57
William Zhang <willzhang05@gmail.com>
2017-12-02 21:53:44 -0500

    constant 100 lines

--- faillog/fetch_journal.sh
+++ faillog/fetch_journal.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 touch sshd.json
-journalctl -q -u sshd --no-pager -o json -n 1 > sshd.json
+journalctl -q -u sshd --no-pager -o json -n 100 > sshd.json
 TIME=$( date +"%T" )
 while true
 do
         LENGTH=$(fgrep -o "{" sshd.json | wc -l)
         NEWDATA=$(journalctl -q -u sshd --no-pager -o json --since $TIME)
         DELTA=$(echo $NEWDATA | fgrep -o "{" | wc -l)
-        if [[ $DELTA -gt 10 ]]; then
-            echo "big"
+        echo $LENGTH
+        echo $DELTA
+        if [[ $DELTA -gt 2 ]]; then
+            head -n $DELTA sshd.json
+            sed -i 1,"$DELTA"d sshd.json
             echo $NEWDATA >> sshd.json
             TIME=$( date +"%T" )
         fi
-        echo $LENGTH
-        echo $DELTA
-        sleep 5
-        clear
+        #if [[ $LENGTH -gt 100 ]]; then
+        #    journalctl -q -u sshd --no-pager -o json -n 100 > sshd.json
+        #fi
+        sleep 2
         
 done
